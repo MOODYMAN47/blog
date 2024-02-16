5c981644e700cfcaef56bb184e4351dcef54ffdd
DESKTOP-LKHO6QF\DellPC <mhmoo.maliil@gmail.com>
2024-02-16 17:41:04 +0300

    first

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function blog(){
+    return view('blog');
+}
+public function post(){
+    return view('layouts.post');
+}
+public function about(){
+    return view('layouts.about');
+}
+
+}
